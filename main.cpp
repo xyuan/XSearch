07a409a50dd326e1eabe9b100bc06cec80b29ff3
WangXiao <wangxiao0722@gmail.com>
2012-03-16 16:05:46 +0800

    update

--- main.cpp
+++ main.cpp
@@ -1,94 +1,11 @@
-#include <iostream>
-#include <stdlib.h>
-#include <pthread.h>
-#include <string>
-#include <sstream>
+#include "url.cpp"
 
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
 using namespace std;
 
-typedef struct ct_sum{
-    int sum;
-    pthread_mutex_t lock;
-} ct_sum;
-
-typedef struct noct_sum{
-    int sum;
-} noct_sum;
-
-void * add_one ( void * cnt ) {
-    pthread_mutex_lock(&(((ct_sum*)cnt)->lock));
-    for ( int i = 0; i < 50; i++ ) {
-        (*(ct_sum*)cnt).sum+=i;
-    }
-    pthread_mutex_unlock(&(((ct_sum*)cnt)->lock));
-    pthread_exit(NULL);
-    return 0;
-}
-
-void * add_two ( void * cnt ) {
-    pthread_mutex_lock(&(((ct_sum*)cnt)->lock));
-    for ( int i = 0; i < 50; i++ ) {
-        (*(ct_sum*)cnt).sum+=i;
-    }
-    pthread_mutex_unlock(&(((ct_sum*)cnt)->lock));
-    pthread_exit(NULL);
-    return 0;
-}
-
-void *add1(void * cnt){
-    for ( int i = 0; i < 50; i++ ) {
-        cout << "enter one" << endl;
-        (*(ct_sum*)cnt).sum+=i;
-    }
-}
-void *add2(void * cnt){
-    for ( int i = 0; i < 50; i++ ) {
-        cout << "enter two" << endl;
-        (*(ct_sum*)cnt).sum+=i;
-    }
-}
-
-string itos(long long i){
-		stringstream s;
-		s << i;
-		return s.str();
-}
 //main function
 int main()
 {
-    in_addr_t temp;
-    temp = inet_addr("211.100.7.101");
-    cout << temp << endl;
-    
-    /*
-    pthread_t tid;
-    tid = pthread_self();
-    char s[64];
-    sprintf(s,"%lu",tid);
-    cout << s << " " << strlen(s);
-    noct_sum cnt;
-    cnt.sum = 0;
-    pthread_t ptid1,ptid2;
-    pthread_create(&ptid1,NULL,add1,&cnt);
-    pthread_create(&ptid2,NULL,add2,&cnt);
-    cout << cnt.sum;
-    pthread_t ptid1,ptid2;
-    int sum=0;
-    ct_sum cnt;
-    pthread_mutex_init(&(cnt.lock),NULL);
-    cnt.sum=0;
-    pthread_create(&ptid1,NULL,add_one,&cnt);
-    pthread_create(&ptid2,NULL,add_two,&cnt);
-    pthread_mutex_lock(&(cnt.lock));
-    printf("sum %d\n",cnt.sum);
-    pthread_mutex_unlock(&(cnt.lock));
-    pthread_join(ptid1,NULL); //等待线程ptid1结束
-    pthread_join(ptid2,NULL); //等待线程ptid2结束
-    printf("sum %d\n",cnt.sum);
-    pthread_mutex_destroy(&(cnt.lock));
-    */
+    Url url;
+    url.parse_url("http://www.baidu.com");
     return 0;
 }
--- url.h
+++ url.h
@@ -1,19 +1,22 @@
+#ifndef _URL_H
+#define _URL_H
 #include <string>
 
 using namespace std;
 
 
-enum url_schema {
-    SCHEME_HTTP;
-    SCHEME_FTP;
-    SCHEME_INVALID;
+enum url_scheme {
+    SCHEME_HTTP,
+    SCHEME_FTP,
+    SCHEME_INVALID,
 };
 
 const unsigned int URL_LEN = 256;
 const unsigned int HOST_LEN = 256;
 const int DEFAULT_HTTP_PORT = 80;
 const int DEFAULT_FTP_PORT = 21;
-
+const char * LEAD_HTTP = "http://";
+const char * LEAD_FTP = "ftp://";
 
 class Url
 {
@@ -23,9 +26,13 @@ public:
 
     bool parse_url ( string url );
 private:
-    string url;
-    string host;
-    string port;
-    string path;
+    void parse_scheme ( const char * url );
+private:
+    enum url_scheme m_scheme;
+    string m_url;
+    string m_host;
+    string m_port;
+    string m_path;
+};
 
-}
+#endif
